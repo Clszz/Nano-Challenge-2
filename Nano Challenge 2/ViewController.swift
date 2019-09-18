adb5f5e8c74e134146d028da614a5efb271351c3
Clsz <muhammad.reynaldi001@binus.ac.id>
2019-09-18 18:59:30 +0700

    Initial Commit

---
+++ Nano Challenge 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nano Challenge 2
+//
+//  Created by Muhammad Reynaldi on 18/09/19.
+//  Copyright © 2019 11. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
